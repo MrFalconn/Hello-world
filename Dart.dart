320e633ba2c9c9ff69b43cddf6cac93df2369712
MrFalconn <87127323+MrFalconn@users.noreply.github.com>
2021-08-01 03:11:05 -0700

    Create Dart.dart

---
+++ Dart.dart
@@ -0,0 +1,9 @@
+<span style="font-size: 16px;">// main() is the entry point of our dart program
+
+main() {
+
+    //print function to print the text to console
+
+   print("Hello World");
+
+}</span>
